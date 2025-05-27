d2954ef76e9111ec8acc617b9e41e6b6169ff9d4
marleydnb123 <marleydnb123@gmail.com>
2025-05-27 23:47:09 +0100

    Updated Directory.tsx

--- src/pages/Directory.tsx
+++ src/pages/Directory.tsx
@@ -185,7 +185,7 @@ const Directory: React.FC = () => {
       results = results.filter(fishery => fishery.tackleHire);
     }
     if (coaching) {
-      results = results.filter( => fishery.coaching);
+      results = results.filter(fishery => fishery.coaching);
     }
     setFilteredFisheries(results);
   }, [

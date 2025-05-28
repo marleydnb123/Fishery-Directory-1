b1f552edbbd05c206ed886467d4e9a0bb9e03d16
marleydnb123 <marleydnb123@gmail.com>
2025-05-28 20:31:24 +0100

    Deploy application to Netlify

--- src/pages/Directory.tsx
+++ src/pages/Directory.tsx
@@ -405,7 +405,7 @@ const Directory: React.FC = () => {
 {/* Carp filters */}
 <div className="grid grid-cols-2 grid-rows-10 gap-2 h-full mt-5">  
   {/* Advanced Filters */}
-<div className="font-bold text-lg mb-3">Advanced Filters</div>
+<div className="font-bold text-lg">Advanced Filters</div>
   <div className="flex items-center gap-2">   
     <input 
       type="checkbox"

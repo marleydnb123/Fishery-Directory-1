6bbf3918915c855793fe66b0b4708ba501bf9089
marleydnb123 <marleydnb123@gmail.com>
2025-05-28 01:25:40 +0100

    Deploy application to Netlify

--- src/pages/FisheryDetail.tsx
+++ src/pages/FisheryDetail.tsx
@@ -331,7 +331,7 @@ const FisheryDetail: React.FC = () => {
             {fishery.features && fishery.features.length > 0 && (
             <div className="mt-8">
             <h3 className="text-lg font-semibold mb-3 flex items-center">
-            <Waves className="h-5 w-5 text-blue-600 mr-2" />
+            <Waves className="h-5 w-5 text-blue-600 mr-2 transition-transform duration-200 hover:scale-[1.02]" />
             Water Features
             </h3>
             <div className="flex flex-wrap gap-2">

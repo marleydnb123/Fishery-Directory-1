3b90f2b40966388510f0f81d27f24c0e38b001da
marleydnb123 <marleydnb123@gmail.com>
2025-05-28 01:34:41 +0100

    Deploy application to Netlify

--- src/pages/FisheryDetail.tsx
+++ src/pages/FisheryDetail.tsx
@@ -842,7 +842,7 @@ rel="noopener noreferrer"
                 initial={{ y: 20, opacity: 0 }}
                 animate={{ y: 0, opacity: 1 }}
                 transition={{ duration: 0.5 }}
-                className="bg-white rounded-xl shadow-md overflow-hidden hover:shadow-xl transition-shadow transition-transform duration-200 hover:scale-[1.08]"
+                className="bg-white rounded-xl transition-transform duration-200 hover:scale-[1.02] shadow-md overflow-hidden hover:shadow-xl transition-shadow"
               >
                 <Link to={`/directory/${f.slug}`}>
                   <img

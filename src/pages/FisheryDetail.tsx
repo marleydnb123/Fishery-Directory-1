a44835a2af2bd2cc1cea441631fb55a1e5eb065f
marleydnb123 <marleydnb123@gmail.com>
2025-05-29 17:09:01 +0100

    Deploy application to Netlify

--- src/pages/FisheryDetail.tsx
+++ src/pages/FisheryDetail.tsx
@@ -724,7 +724,9 @@ const FisheryDetail: React.FC = () => {
             <span className="font-semibold text-primary-700">Address:</span> {fishery.address || "Not listed"}
           </li>
         </ul>
-        
+        <div className="mt-2 mb-2 text-xs text-primary-500 italic">
+          Fast replies & friendly staff. We do not handle bookings directly, contact the Fishery directly to book.
+        </div> 
       </div>
     </div>
   </div>

28b9c39c9a7b2aa642486de78469df44ede5589d
marleydnb123 <marleydnb123@gmail.com>
2025-05-29 18:49:58 +0100

    Updated Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -233,7 +233,7 @@ const Home: React.FC = () => {
       
       {/* === Vertical Blue Divider === */}
       <div className="hidden md:flex items-center px-0">
-        <div className="h-300 w-1 bg-blue-700 rounded-full mx-auto" style={{ minHeight: '400px' }}></div>
+        <div className="h-300 w-0.5 bg-blue-700/50 rounded-full mx-auto" style={{ minHeight: '400px' }}></div>
       </div>
       
       {/* === Catch of the Week Card === */}

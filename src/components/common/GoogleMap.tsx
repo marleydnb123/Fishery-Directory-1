4b3f8b064b8b2b0387beedfedbb5b9ba41899c28
marleydnb123 <marleydnb123@gmail.com>
2025-05-30 00:27:47 +0100

    Deploy application to Netlify

--- src/components/common/GoogleMap.tsx
+++ src/components/common/GoogleMap.tsx
@@ -39,7 +39,7 @@ const GoogleMap: React.FC<GoogleMapProps> = ({ latitude, longitude, name }) => {
       apiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY || "YOUR_GOOGLE_MAPS_API_KEY"}
       onError={() => setMapError(true)}
     >
-      <div className="min-h-[400px] h-auto rounded-lg overflow-hidden mb-6"> 
+      <div className="h-[400px] md:h-[400px] rounded-lg overflow-hidden">  
         <Map
           defaultCenter={{ lat: latitude, lng: longitude }}
           defaultZoom={14}

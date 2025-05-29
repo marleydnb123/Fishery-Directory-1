4463a432d3ebd02d74a728021c13946c5d6b81d9
marleydnb123 <marleydnb123@gmail.com>
2025-05-29 19:01:21 +0100

    Updated Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -222,7 +222,7 @@ const Home: React.FC = () => {
         <h3 className="text-3xl font-bebas font-bold text-gray-900 mb-2 text-center">
           Fishery of the Week
         </h3>
-        <div className="bg-blue-50 rounded-2xl shadow-lg p-6 h-full flex flex-col justify-centre">
+        <div className="bg-blue-50 rounded-2xl shadow-lg p-6 h-full flex flex-col justify-center">
           {featuredFisheries.length > 0 ? (
             <FisheryCard fishery={featuredFisheries[0]} />
           ) : (

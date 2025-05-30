c212cd91886cd12559ab99c19de61aca3e92e6bf
marleydnb123 <marleydnb123@gmail.com>
2025-05-30 20:24:54 +0100

    Updated Directory.tsx

--- src/pages/Directory.tsx
+++ src/pages/Directory.tsx
@@ -416,7 +416,7 @@ const Directory: React.FC = () => {
                 <button
                   type="button"
                   onClick={() => setCarpOpen(!carpOpen)}
-                  className="w-full flex items-center justify-between p-3 rounded-xl text-left hover:bg-customBlue/50 hover:rounded-xl transition-colors mb-2"
+                  className="w-full flex items-center justify-between p-3 rounded-xl text-left hover:bg-customBlue/50 hover:rounded-xl transition-colors"
                 >
                   <h3 className="text-sm font-bold text-gray-800">CARP</h3>
                   <svg

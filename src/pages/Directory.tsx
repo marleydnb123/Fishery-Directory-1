9f22bb9b23c4319bae07592169277573b6b20fbb
marleydnb123 <marleydnb123@gmail.com>
2025-05-31 15:33:20 +0100

    Updated Directory.tsx

--- src/pages/Directory.tsx
+++ src/pages/Directory.tsx
@@ -498,7 +498,7 @@ const Directory: React.FC = () => {
                 <button
                   type="button"
                   onClick={() => setCarpOpen(!carpOpen)}
-                  className="w-full flex items-center justify-between p-3 rounded-xl text-left hover:bg-customBlue/50 hover:rounded-xl transition-colors"
+                  className="w-full flex items-center justify-between p-3 rounded-xl text-left hover:bg-customBlue/10 hover:rounded-xl transition-colors"
                 >
                   <h3 className="text-sm font-bold text-gray-800">CARP</h3>
                   <svg
@@ -555,7 +555,7 @@ const Directory: React.FC = () => {
                 <button
                   type="button"
                   onClick={() => setMatchOpen(!matchOpen)}
-                  className="w-full flex items-center justify-between p-3 text-left rounded-xl text-left hover:bg-customBlue/50 hover:rounded-xl  transition-colors"
+                  className="w-full flex items-center justify-between p-3 text-left rounded-xl text-left hover:bg-customBlue/10 hover:rounded-xl  transition-colors"
                 >
                   <h3 className="text-sm font-bold text-gray-800">MATCH</h3>
                   <svg

8f5b2f07364c498fca51016d3350555a33c10a1e
marleydnb123 <marleydnb123@gmail.com>
2025-05-31 15:50:44 +0100

    Updated Directory.tsx

--- src/pages/Directory.tsx
+++ src/pages/Directory.tsx
@@ -456,11 +456,11 @@ const Directory: React.FC = () => {
               </div>
             </div>
 
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+            <div className="flex flex-col gap-3">
               <h3 className="block text-xs font-semibold text-gray-600">
                 Advanced Filters
               </h3>
-
+ 
 
               {/* CHECKBOXES FILTERS */}
               {/* GENERAL FACTS FILTERS */}

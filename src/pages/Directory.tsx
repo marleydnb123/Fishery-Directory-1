1b6544a73e18b10c51b8549ccb1e79203d1068f0
marleydnb123 <marleydnb123@gmail.com>
2025-05-31 14:14:24 +0100

    Updated Directory.tsx

--- src/pages/Directory.tsx
+++ src/pages/Directory.tsx
@@ -21,7 +21,7 @@ const Directory: React.FC = () => {
   const [dogFriendly, setDogFriendly] = useState(false);
   const [priceRange, setPriceRange] = useState('');
   const [firePitsAllowed, setFirePitsAllowed] = useState(false);
-  const [selectedBookingType, setBookingType] = useState('');
+  const [selectedbookingType, setbookingType] = useState('');
   const [parkingClose, setParkingClose] = useState(false); 
   const [campingAllowed, setCampingAllowed] = useState(false);
   const [catchPhotos, setCatchPhotos] = useState(false); 
